186bfc7e2365f63059bfc8687db3f685783b2965
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2021-05-17 16:19:02 +0200

    fix info

--- ngrok_info/__init__.py
+++ ngrok_info/__init__.py
@@ -10,13 +10,15 @@ import time
 #!                              !
 #!------------------------------!
 
+lvers = "1.0.7"
+
 
 def checkver:
     packagenm = 'ngrok_info'  # replace with the package you want to check
     responseinfl = requests.get(f'https://pypi.org/pypi/{packagenm}/json')
     latest_version = responseinfl.json()['info']['version']
-    if latest_version != 1.0.6:
-        print("You are not using latest version, run 'python3 -m pip install ngrok_info'")
+    if latest_version != lvers:
+        print("You are not using latest version, run 'python3 -m pip install ngrok_info --upgrade'")
 
 checkver()
 
