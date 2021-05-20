00df74c78cd1b4ed9f25900e5b42fe97b59b5708
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2021-05-20 19:26:18 +0200

    More readme.md updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ngrok_info',
-      version='1.1.14',
+      version='1.1.15',
       description='Easy and fast tool written in python 3 to get info about running ngrok tunnel',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
